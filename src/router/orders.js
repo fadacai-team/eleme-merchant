4a5e272f1696a5acfe808abeb1f1d1fef98be811
chamsonxie <chamson1995@qq.com>
2020-08-07 09:44:13 +0800

    8.7.1

--- src/router/orders.js
+++ src/router/orders.js
@@ -3,36 +3,40 @@ export default[
         path: '/orders',
         component: () => import ('../views/Orders/OrderProcess.vue'),
         name: 'oprocess',
+        children:[
+            {
+                path: 'property',
+                component: () => import ('../views/Orders/PropertyCenter.vue'),
+                name: 'property',
+            },
+            {
+                path: 'property/cantcompe',
+                component: () => import ('../components/order/cantCompensate.vue'),
+                name: 'cantcompe',
+            },
+            {
+                path: 'process',
+                component: () => import ('../views/Orders/OrderProcess.vue'),
+                name: 'oprocess',
+            },
+            {
+                path: 'search',
+                component: () => import ('../views/Orders/OrderSearch.vue'),
+                name: 'osearch',
+            },
+            {
+                path: 'setting',
+                component: () => import ('../views/Orders/OrderSetting.vue'),
+                name: 'osetting',
+            },
+            {
+                path: 'reservation',
+                component: () => import ('../views/Orders/Reservation.vue'),
+                name: 'oreservation',
+            }
+
+        ]
     },
-    {
-        path: '/orders/property',
-        component: () => import ('../views/Orders/PropertyCenter.vue'),
-        name: 'property',
-    },
-    {
-        path: '/orders/property/cantcompe',
-        component: () => import ('../components/order/cantCompensate.vue'),
-        name: 'cantcompe',
-    },
-    {
-        path: '/orders/process',
-        component: () => import ('../views/Orders/OrderProcess.vue'),
-        name: 'oprocess',
-    },
-    {
-        path: '/orders/search',
-        component: () => import ('../views/Orders/OrderSearch.vue'),
-        name: 'osearch',
-    },
-    {
-        path: '/orders/setting',
-        component: () => import ('../views/Orders/OrderSetting.vue'),
-        name: 'osetting',
-    },
-    {
-        path: '/orders/reservation',
-        component: () => import ('../views/Orders/Reservation.vue'),
-        name: 'oreservation',
-    }
+    
     
 ]
